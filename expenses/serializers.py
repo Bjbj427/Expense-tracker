eaabd5e0e84518ad4cd725ac68067e5c23acab42
Babajide Bello <abdulwalibello@gmail.com>
2026-09-15 20:22:38 +0100

    Full expense tracker project

---
+++ expenses/serializers.py
@@ -0,0 +1,16 @@
+from rest_framework import serializers
+from .models import Category, Expense
+
+
+class CategorySerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Category
+        fields = '__all__'
+
+
+class ExpenseSerializer(serializers.ModelSerializer):
+    category_name = serializers.CharField(source='category.name', read_only=True)
+
+    class Meta:
+        model = Expense
+        fields = ['id', 'title', 'amount', 'category', 'category_name', 'date', 'notes']
